f89a02aeb391cfbe527d75b3f33a0391948a07c9
wasilevskiy <vasilevskii_ilya@mail.ru>
2017-04-19 18:10:44 +0300

    f

--- EssentialTools/Models/Product.cs
+++ EssentialTools/Models/Product.cs
@@ -10,7 +10,6 @@ namespace EssentialTools.Models
         public int ProductID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-
         public decimal Price { get; set; }
         public string Category { get; set; }
     }
